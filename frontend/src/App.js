d3756331f2e3e3d9a6d6e6cfdb97214ca1784986
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-02-08 17:26:21 +0000

    Create landing page with hero image and make responsive.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import LandingPage from "./screens/Landingpage/LandingPage";
 
 
 const App = () => (
     <>
     <Header />
     <main style={{minHeight: "93vh"}}>
+        <LandingPage />
         </main>
     <Footer />
 
