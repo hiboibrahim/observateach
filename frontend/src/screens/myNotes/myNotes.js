efb03f40dd796adfbbe6181ed8468724d0b4b509
hibo ibrahim <hibo.ibrahim@gmail.com>
2024-03-06 17:48:47 +0000

    notes pages working

--- frontend/src/screens/myNotes/myNotes.js
+++ frontend/src/screens/myNotes/myNotes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import { Accordion, Badge, Button, Card, useAccordionButton } from "react-bootstrap";
 import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
 import notes from "../../data/notes";
@@ -9,9 +9,103 @@ const MyNotes = () => {
     if (window.confirm("Are you sure?")) {
     }
   };
+
+  const decoratedOnClick = useAccordionButton(() =>
+    console.log('totally custom!'),
+);
+
   return (
     <div>
-      <h2>Hello mynotes</h2>
+      <MainScreen title={"Welcome back Hibo Ibrahim"}>
+        My Observations
+        <Link to="/createnote">
+            <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+              Create Observation
+            </Button>
+        </Link>
+        {notes.map((note, index) => {
+          console.log(note, 'note dataaa')
+          return(
+            <>
+              <Accordion>
+                <Card style={{ margin: 10 }}>
+                  <Card.Header style={{ display: "flex" }}>
+                    <span
+                      style={{
+                        color: "black",
+                        textDecoration: "none",
+                        flex: 1,
+                        cursor: "pointer",
+                        alignSelf: "center",
+                        fontSize: 18,
+                      }}
+                    >
+                      <button
+                        type="button"
+                        style={{ backgroundColor: 'pink' }}
+                        onClick={()=>decoratedOnClick(0)}
+                      >
+                        {note.title}
+                      </button>
+                      {note.title}
+                    </span>
+
+                    <div>
+                      <Button href={`/note/${note._id}`}>Edit</Button>
+                      <Button
+                        variant="danger"
+                        className="mx-2"
+                        onClick={() => deleteHandler(note._id)}
+                      >
+                        Delete
+                      </Button>
+                  </div>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                      <h4>
+                        <Badge variant="success">Subject -{note.subject} </Badge>
+                      </h4>
+                      <blockquote className="blockquote mb-0">
+                        <p>{note.content}</p>
+                        <footer className="blockquote-footer">
+                          Created on - date
+                        </footer>
+                      </blockquote>
+                    </Card.Body>
+                </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </>
+          )
+        })}
+          <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                // onClick={() => ModelShow(note)}
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              ></span>
+
+              <div>
+                <Button>Edit</Button>
+                <Button variant="danger" className="mx-2">
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+          </Card>
+
+
+      </MainScreen>
+      
+
       {/* <MainScreen title="Welcome back Hibo Ibrahim">
         My Observations
         <Link to="/createnote">
